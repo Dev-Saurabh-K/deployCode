4084c00b9570b246b150ce2b8e14976b382ed3b4
dev-saurabh-k <saurabhkumar.sakr@gmail.com>
2026-08-21 12:22:25 +0530

    sample code

---
+++ sample/sample1/src/App.jsx
@@ -0,0 +1,12 @@
+
+function App() {
+ 
+
+  return (
+    <>
+      <h1>hii this is app1</h1>
+    </>
+  )
+}
+
+export default App
---
+++ sample/sample2/src/App.jsx
@@ -0,0 +1,13 @@
+
+
+function App() {
+
+
+  return (
+    <>
+      <h1>this is app 2 </h1>
+    </>
+  )
+}
+
+export default App
